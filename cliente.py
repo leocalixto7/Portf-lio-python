16bcdffa988da7bd696a9fb99f9001024323ae11
Leonardo Calixto <110504275+leocalixto7@users.noreply.github.com>
2022-11-01 20:25:15 -0300

    Delete cliente.py

--- cliente.py
+++
@@ -1,7 +0,0 @@
-class Cliente:
-    def __init__(self, nome, telefone):
-        self.nome = nome
-        self.telefone = telefone
-        
-    def get_dados(self):
-        return f'Nome: {self.nome} Tel: {self.telefone}'
